6360c8f42f7d8e5d06582a9f35ac60652157041e
Divya Sharma <120268869+divyasharma11@users.noreply.github.com>
2023-07-20 18:56:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,30 +2,19 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
-// Parsing user data from user.json 
 const users = JSON.parse(fs.readFileSync(`${__dirname}/../data/users.json`));
 
 app.use(express.json());
 
-/*
-Write a GET Request to return all users. 
-The response should be in the following format: 
-{
-    "status": "success",
-    "data": {
-        "users": [
-            {
-                "_id": 1,
-                "name": "James B",
-                "email": "jamesb@example.com"
-            },
-            ...
-        ]
-    }
-}*/
+//Get all users
 app.get("/api/v1/users/", (req, res) => {
     try {
-        //Write your code here.
+        res.status(201).json({
+            status: "success",
+            data: {
+                users,
+            },
+        });
     } catch (err) {
         res.status(404).json({
             message: "Users Not Found",
@@ -35,25 +24,23 @@ app.get("/api/v1/users/", (req, res) => {
     }
 });
 
-
-/*
-Write a GET Request to return a user by ID. 
-The response should be in the following format: 
-{
-    "status": "success",
-    "data": {
-        "user": {
-            "_id": 1,
-            "name": "James B",
-            "email": "jamesb@example.com"
-        }
-    }
-}
-Return 404 error when user is not found. 
-*/
+//Get user by ID
 app.get("/api/v1/users/:id", (req, res) => {
     try {
-        //Write your code here
+        const user = users.find(obj => obj._id == req.params.id);
+        if (!user) {
+            return res.status(404).json({
+                status: "Error",
+                message: "User Not Found"
+            });
+        }
+        return res.status(201).json({
+            status: "success",
+            data: {
+                user: user
+            }
+        })
+
     } catch (err) {
         res.status(400).json({
             message: "User Fetching Failed",
@@ -63,26 +50,40 @@ app.get("/api/v1/users/:id", (req, res) => {
     }
 });
 
-
-/*
-Write a POST request to create a new User. 
-The response should be in the following format: 
-{
-    "status": "success",
-    "data": {
-        "user": {
-            "_id": 5,
-            "name": "Someone Someone",
-            "email": "someone@gmail.com"
-        }
-    }
-}
-Generate a new id using the id of the last user in the database, increment it by 1
-Return a 400 error when the email or name is missing 
-*/
+// Create new User
 app.post("/api/v1/users/", (req, res) => {
     try {
-        //Write your code here
+        const { name, email } = req.body;
+
+        if (!name) {
+            return res.status(400).json({
+                message: "Name Missing",
+                status: "Error",
+            });
+        }
+        if (!email) {
+            return res.status(400).json({
+                message: "Email Missing",
+                status: "Error",
+            });
+        }
+
+        const new_id = users[users.length - 1]._id + 1;
+        const new_user = { _id: new_id, name, email };
+        users.push(new_user);
+
+        fs.writeFile(
+            `${__dirname}/../data/users.json`,
+            JSON.stringify(users),
+            (err) => {
+                res.status(201).json({
+                    status: "success",
+                    data: {
+                        user: new_user,
+                    },
+                });
+            }
+        );
     } catch (err) {
         res.status(400).json({
             message: "User Creation failed",
@@ -91,35 +92,34 @@ app.post("/api/v1/users/", (req, res) => {
     }
 });
 
-/*
-Write a PATCH request to update user's name or email. 
-The response should be in the following format: 
-{
-    "status": "success",
-    "data": {
-        "users": [
-            {
-                "_id": 1,
-                "name": "James A",
-                "email": "jamesA@example.com"
-            },
-            {
-                "_id": 2,
-                "name": "James B",
-                "email": "jamesb@example.com"
-            }, ....
-        ]
-    }
-}
-req.body can contain both name and email as well. Update the data based on the parameters recieved in req.body
-Return a 404 error if the user is missing, with the following message 
-{
-    "message": "User not Found"
-}
-*/
+// Update User
 app.patch("/api/v1/users/:id", (req, res) => {
     try {
-        //Write your code here.
+        const { name, email } = req.body;
+        for (var i in users) {
+            if (users[i]._id == req.params.id) {
+                users[i].name = name || users[i].name;
+                users[i].email = email || users[i].email;
+
+                fs.writeFile(
+                    `${__dirname}/../data/users.json`,
+                    JSON.stringify(users),
+                    (err) => {
+                        res.status(201).json({
+                            status: "success",
+                            data: {
+                                users,
+                            },
+                        });
+                    }
+                );
+                return;
+            }
+        }
+        res.status(404).json({
+            message: "User Not Found",
+            status: "Error",
+        });
     } catch (err) {
         console.log(err);
         res.status(400).json({
@@ -130,43 +130,26 @@ app.patch("/api/v1/users/:id", (req, res) => {
     }
 });
 
-/*
-Write a DELETE request to delete the user from the users.json
-The response should be in the following format: 
-{
-    "status": "success",
-    "data": {
-        "users": [
-            {
-                "_id": 1,
-                "name": "James A",
-                "email": "jamesA@example.com"
-            },
-            {
-                "_id": 2,
-                "name": "James B",
-                "email": "jamesb@example.com"
-            }, .... // Shouldn't have the user deleted.
-        ]
-    }
-}
-
-Return a 404 error if the user is missing, with the following message 
-{
-    "message": "User not Found"
-}
-*/
+// Delete User
 app.delete("/api/v1/users/:id", (req, res) => {
-    try {
-        //Write your code here.
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({
-            message: "User Deletion Failed",
-            status: "Error",
-            error: err,
-        });
+    const object = users.find(obj => obj._id == req.params.id);
+    if (!object) {
+        return res.status(404).json({ status: "Error", message: "User not Found" });
     }
+    const filteredUsers = users.filter((item) => item._id != req.params.id);
+    fs.writeFile(
+        `${__dirname}/../data/users.json`,
+        JSON.stringify(filteredUsers),
+        (err) => {
+            res.status(201).json({
+                status: "success",
+                data: {
+                    users: filteredUsers,
+                },
+            });
+        }
+    );
+    return;
 });
 
 module.exports = app;
